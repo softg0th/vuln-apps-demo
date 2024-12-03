edcbd29f46ee5f266cef8dd850f16b1d4d70ac06
Nikolai Nesterenko <nesni248@gmail.com>
2024-12-03 10:01:14 +0300

    kotlin done

--- sqli/orm/src/main/kotlin/com/example/orm/api/HttpHandlers.kt
+++ sqli/orm/src/main/kotlin/com/example/orm/api/HttpHandlers.kt
@@ -1,23 +1,28 @@
 package com.example.orm.api
 
-import org.springframework.stereotype.Controller
 import com.example.orm.entity.User
 import com.example.orm.service.UserService
 import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
 @Controller
 @RequestMapping("/")
-class UserController(
-    private val userService: UserService
-) {
+class MainController {
     @GetMapping
     fun index(): String {
         return "index"
     }
+}
 
+@RestController
+@RequestMapping("/api")
+class UserController(
+    private val userService: UserService
+) {
     @PostMapping("/select_data")
     fun selectData(@RequestParam("user_prompt") userPrompt: String): ResponseEntity<List<User>> {
+        // Возвращает данные в формате JSON
         val users = userService.getUsersByName(userPrompt)
         return ResponseEntity.ok(users)
     }
