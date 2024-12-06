45b54e8d8fe6b9b7214c1d03befa56a06c3d5799
softg0th <67077296+softg0th@users.noreply.github.com>
2024-12-06 13:03:59 +0300

    Update UserRepository.kt

--- sqli/orm/src/main/kotlin/com/example/orm/repository/UserRepository.kt
+++ sqli/orm/src/main/kotlin/com/example/orm/repository/UserRepository.kt
@@ -8,3 +8,16 @@ import org.springframework.stereotype.Repository
 interface UserRepository : JpaRepository<User, Long> {
     fun findByUsername(username: String): List<User>
 }
+
+
+/*
+@Repository("userCustomRepositoryImpl")
+class UserCustomRepositoryImpl(
+    @PersistenceContext private val entityManager: EntityManager
+) : UserCustomRepository {
+    override fun unsafeFindUsersByName(name: String): List<User> {
+        val query = "SELECT u FROM User u WHERE u.name = '$name'"
+        return entityManager.createQuery(query, User::class.java).resultList
+    }
+}
+*/
